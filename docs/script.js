53eab19d969b3b1693d79826344bd286a8f8e7c3
Het Suthar <hetsuthar2001@gmail.com>
2021-06-05 15:33:33 +0530

    Added Counter

--- docs/script.js
+++ docs/script.js
@@ -1,30 +1,44 @@
-function computerPlay(){
-    let computerMoves = ['ROCK','PAPER','SCISSOR'];
-    let rand = Math.floor(Math.random() * 3);
-    return computerMoves[rand];
-}
-function playRound(playerSelection,computerSelection){
-    if(playerSelection == 'ROCK'){
-        if(computerSelection == 'ROCK') return 'TIE, both are ROCKS!!';
-        if(computerSelection == 'PAPER') return 'You Lost!!';
-        if(computerSelection == 'SCISSOR') return 'You Won!!';
+function game(){
+    function computerPlay(){
+        let computerMoves = ['ROCK','PAPER','SCISSOR'];
+        let rand = Math.floor(Math.random() * 3);
+        return computerMoves[rand];
     }
-    else if(playerSelection == 'PAPER'){
-        if(computerSelection == 'ROCK') return 'You Won!!';
-        if(computerSelection == 'PAPER') return 'TIE, both are PAPERS!!';
-        if(computerSelection == 'SCISSOR') return 'You Lost!!';
+    function playRound(playerSelection,computerSelection){
+        if(playerSelection == 'ROCK'){
+            if(computerSelection == 'ROCK') {++tieCount; return 'TIE, both Are SCISSORS!!';}
+            if(computerSelection == 'PAPER') {++computerCount; return 'You Lost!!';}
+            if(computerSelection == 'SCISSOR') {++playerCount; return 'You Won!!';}
+        }
+        else if(playerSelection == 'PAPER'){
+            if(computerSelection == 'ROCK') {++playerCount; return 'You Won!!';}
+            if(computerSelection == 'PAPER') {++tieCount; return 'TIE, both Are SCISSORS!!';}
+            if(computerSelection == 'SCISSOR') {++computerCount; return 'You Lost!!';}
+        }
+        else if(playerSelection == 'SCISSOR'){
+            if(computerSelection == 'ROCK') {++computerCount; return 'You Lost!!';}
+            if(computerSelection == 'PAPER') {++playerCount; return 'You Won!!';}
+            if(computerSelection == 'SCISSOR') {++tieCount; return 'TIE, both Are SCISSORS!!';}
+        }
+        else{
+            return 'Invalid Input!!'
+        }
     }
-    else if(playerSelection == 'SCISSOR'){
-        if(computerSelection == 'ROCK') return 'You Lost!!';
-        if(computerSelection == 'PAPER') return 'You Won!!';
-        if(computerSelection == 'SCISSOR') return 'TIE, both Are SCISSORS!!';
-    }
-    else{
-        return 'Invalid Input!!'
+    let computerCount = 0;
+    let playerCount = 0;
+    let tieCount = 0;
+    for(let i=0;i<5;i++){    
+        const playerSelection = prompt('Enter Your Move').toUpperCase();
+        const computerSelection = computerPlay();
+        console.log('You played:'+playerSelection+', Computer played:'+computerSelection+'. this round '+playRound(playerSelection,computerSelection));
+        console.log();
+    }  
+    console.log('You won '+playerCount+' time, Computer won '+computerCount+' time. Tie '+tieCount+' time');
+    if(playerCount > computerCount){
+        console.log('YOU WON!!');
+    } else {
+        console.log('YOU LOST!!');
     }
 }
 
-const playerSelection = prompt('Enter Your Move').toUpperCase();
-const computerSelection = computerPlay();
-console.log('You played:'+playerSelection+', Computer played:'+computerSelection);
-console.log(playRound(playerSelection,computerSelection));
+game();
