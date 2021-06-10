2a8c669eeb9fa9dc9494886612bf379f8d75cf4b
Het Suthar <hetsuthar2001@gmail.com>
2021-06-10 09:35:14 +0530

    Added basic UI and css

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+/*
 function game(){
     function computerPlay(){
         let computerMoves = ['ROCK','PAPER','SCISSOR'];
@@ -42,3 +43,95 @@ function game(){
 }
 
 game();
+
+*/
+function computerPlay(){
+    let computerMoves = ['ROCK','PAPER','SCISSOR'];
+    let rand = Math.floor(Math.random() * 3);
+    return computerMoves[rand];
+}
+/*
+let computerCount = 0;
+let playerCount = 0;
+let tieCount = 0;
+let counter = 0;
+const cCount = document.getElementById('computercount');
+const pCount = document.getElementById('playercount');
+const tCount = document.getElementById('tiecount');
+const winner = document.getElementById('winner');
+cCount.innerHTML = 'Computer : '+computerCount;
+pCount.innerHTML = 'You : '+playerCount;
+tCount.innerHTML = 'Tie : '+tieCount;
+*/
+        
+const btn = document.querySelectorAll('button');
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        function playRound(playerSelection,computerSelection){
+            const img1 = document.getElementById('playermove');
+            const img2 = document.getElementById('computermove');
+            if(playerSelection == 'ROCK'){
+                img1.src = 'Images/rock.JPG';
+                if(computerSelection == 'ROCK') {
+                    //tieCount++;
+                    img2.src = "Images/rock.JPG"; 
+                    return 'TIE, both Are ROCKS!!';
+                }
+                if(computerSelection == 'PAPER') {
+                    //computerCount++; 
+                    img2.src = "Images/paper.JPG";
+                    return 'You Lost!!';
+                }
+                if(computerSelection == 'SCISSOR') {
+                    //playerCount++; 
+                    img2.src = "Images/scissor.JPG";
+                    return 'You Won!!';
+                }
+            }
+            else if(playerSelection == 'PAPER'){
+                img1.src = "Images/paper.JPG";
+                if(computerSelection == 'ROCK') {
+                    //playerCount++; 
+                    img2.src = "Images/rock.JPG";
+                    return 'You Won!!';
+                }
+                if(computerSelection == 'PAPER') {
+                    //tieCount++; 
+                    img2.src = "Images/paper.JPG";
+                    return 'TIE, both Are PAPERS!!';
+                }
+                if(computerSelection == 'SCISSOR') {
+                    //computerCount++; 
+                    img2.src = "Images/scissor.JPG";
+                    return 'You Lost!!';
+                }
+            }
+            else if(playerSelection == 'SCISSOR'){
+                img1.src = "Images/scissor.JPG";
+                if(computerSelection == 'ROCK') {
+                    //computerCount++; 
+                    img2.src = "Images/rock.JPG";
+                    return 'You Lost!!';
+                }
+                if(computerSelection == 'PAPER') {
+                    //playerCount++;
+                    img2.src = "Images/paper.JPG";
+                    return 'You Won!!';
+                }
+                if(computerSelection == 'SCISSOR') {
+                    //tieCount++; 
+                    img2.src = "Images/scissor.JPG";
+                    return 'TIE, both Are SCISSORS!!';
+                }
+            }
+            else{
+                return 'Invalid Input!!'
+            }
+        }
+            const playerSelection = button.id.toUpperCase();
+            const computerSelection = computerPlay();
+            const ans = document.getElementById('ans');
+            const answer = playRound(playerSelection,computerSelection);
+            ans.innerHTML = 'this round <b>'+answer+'</b>';
+    });    
+});
